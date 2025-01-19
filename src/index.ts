0a221191be0a60d2809f2b1f575c1d308bf76c81
Pritthish Nath <pritthishnath@gmail.com>
2025-01-19 20:29:22 +0530

    Changing DNS server port

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { config } from "./config";
 import { connectDB, disconnectDB } from "./db/connection";
 import { createDNSServer } from "./dns/server";
 import { createAPIServer } from "./api/routes";
+import { UserModel } from "./models/User";
+import { DNSRecordModel } from "./models/DNSRecord";
 
 async function main() {
   // Connect to MongoDB
@@ -15,6 +17,17 @@ async function main() {
   apiServer.listen(config.API_PORT, () => {
     console.log(`API Server running on port ${config.API_PORT}`);
   });
+
+  // const user = await UserModel.create({
+  //   email: "abcd@example.com",
+  // });
+
+  // await DNSRecordModel.create({
+  //   subdomain: "first4",
+  //   type: "CNAME",
+  //   value: "4.pnath.in",
+  //   userId: user._id,
+  // });
 
   // Handle shutdown
   process.on("SIGINT", async () => {
